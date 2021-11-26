bed43eb5c1918bd3901d31cf7eaea0c0501cc054
MilkaChokolate <milenapolyanichko@yandex.ru>
2021-11-26 14:55:36 +0300

    25.11.21 отправка запрорса на добавлние в БД

--- src/Registrotion.js
+++ src/Registrotion.js
@@ -12,16 +12,26 @@ function Registration(props){
 
     const handleInputChange = (e) => {
         const {value} = e.target;
-        const name = e.target.getAttribute('name');
-        console.log(value, name);
+        const name = e.target.getAttribute("name");
         setFormState({...formState, [name]: value});
     }
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = async (e) =>{
         e.preventDefault();
         e.stopPropagation();
-
         console.log(formState)
+
+        const resp = await fetch('http://localhost:3012/Registration', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=utf-8', 'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(
+                formState
+            )
+        });//запрос на сервер для добавление юзера в БД
+        let commits = await resp.json();
+        console.log(commits)
     }
 
     return (
@@ -32,11 +42,11 @@ function Registration(props){
                 <div id='form'>
                     <form onSubmit={handleSubmit}>
                         <h1>Регистрация</h1>
-                        <Input label="Username" type='text' onChange={handleInputChange} value={formState.username || ''}  name="username" required/>
-                        <Input label='Password' type='password' onChange={handleInputChange}  name="password"  required/>
-                        <Input label='Repeat password' type='password'  onChange={handleInputChange}  name="re_password"   required/>
-                        <button type='submit'>Зарегистрироваться</button>
-                        <div id='hrefReg'><a>Авторизироваться</a></div>
+                        <Input label="Username" type='text' onChange={handleInputChange} value={formState.username || ""}  name="username" required/>
+                        <Input label="Password" type="password" onChange={handleInputChange}  name="password"  required/>
+                        <Input label="Repeat password" type="password"  onChange={handleInputChange}  name="re_password"   required/>
+                        <button type="submit">Зарегистрироваться</button>
+                        <div id='hrefReg'><a href=''>Авторизироваться</a></div>
                     </form>
                 </div>
             </div>
